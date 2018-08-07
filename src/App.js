9b76d3ccc51ebb7cd07d19b28d18504f44f58229
Tyler Haun <frodo03211@gmail.com>
2018-08-07 02:00:03 -0700

    refactored; switched to jsencrypt for key generation; added blinding; added key auto creation + local storage

--- src/App.js
+++ src/App.js
@@ -4,74 +4,150 @@ import './App.css';
 
 import Calendar from "react-calendar";
 
-import crypto from "crypto";
-import keypair from "keypair";
-import moment from "moment";
+import {getOrCreateRSAKeys, PublicKeyBlinder, fetchTimeSlots, bookAppointment} from "./utils";
 
-window.crypto2 = crypto;
-window.keypair = keypair;
+import moment from "moment";
+import JSEncrypt from "jsencrypt";
+
+
 window.moment = moment;
+window.JSEncrypt = JSEncrypt;
+
+
+//let DEFAULT_KEY_SIZE=2048;
+let LOCAL_STORAGE_KEY="SPSRsaKeys";
+
 
 class App extends Component {
 
     constructor(props) {
         super(props);
 
+        var keys = getOrCreateRSAKeys();
+        console.log("GOT OR CREATED KEYS", keys);
+
+
         this.state = {
+            keys: keys
         };
 
-        this.generateKeyPair = this.generateKeyPair.bind(this);
+        //this.generateKeys = this.generateKeys.bind(this);
+        //this.blindPublicKey = this.blindPublicKey.bind(this);
+
+        this.clearKeys = this.clearKeys.bind(this);
+
+
     }
 
-    generateKeyPair() {
-        console.log("GENERATE KEY PAIR");
-        var keys = keypair();
-        console.log(keys);
-        this.setState((previousState) => {
+    clearKeys() {
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+        this.setState(previousState => {
             var newState = previousState;
-            newState.keys = keys;
+            delete newState.keys;
             return newState;
         })
     }
+
+    //getOrCreateKeys() {
+    //    var keys;
+    //    keys = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    //    if (keys) {
+    //        console.log("FOUND KEYS IN LOCAL STORAGE");
+    //    }
+    //    if (!keys) {
+    //        console.log("NO KEYS FOUND; CREATING SOME");
+    //        var jsencrypt = new JSEncrypt({"default_key_size": DEFAULT_KEY_SIZE})
+    //        keys = {
+    //            public: jsencrypt.getPublicKey(),
+    //            private: jsencrypt.getPrivateKey()
+    //        };
+    //        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(keys));
+    //    }
+    //    return keys;
+    //}
+
+    //generateKeys() {
+    //    console.log("GENERATE KEY PAIR");
+    //    var jsencrypt = new JSEncrypt({"default_key_size": DEFAULT_KEY_SIZE})
+    //    var keys = {
+    //        public: jsencrypt.getPublicKey(),
+    //        private: jsencrypt.getPrivateKey()
+    //    };
+    //    console.log(jsencrypt);
+    //    console.log(jsencrypt.key.e);
+    //    console.log(jsencrypt.key.n);
+    //    this.setState((previousState) => {
+    //        var newState = previousState;
+    //        newState.keys = keys;
+    //        newState.jsencrypt = jsencrypt;
+    //        return newState;
+    //    })
+    //    return keys;
+    //}
+
+    //blindPublicKey() {
+    //    console.log("blindPublicKey()");
+    //    var message = this.state.keys.public;
+    //    var N = this.state.jsencrypt.key.n.toString();
+    //    var E = this.state.jsencrypt.key.e.toString();
+    //    const { blinded, r } = BlindSignature.blind({
+    //        message: message,
+    //        N: N,
+    //        E: E
+    //    });
+    //    this.setState(previousState => {
+    //        var newState = previousState;
+    //        newState.blinded
+    //        return newState;
+    //    });
+    //    //console.log("BLINDED", blinded);
+    //    //console.log("r", r);
+    //    //var unblinded = BlindSignature.unblind({
+    //    //    signed: blinded,
+    //    //    N: N,
+    //    //    r: r
+    //    //});
+    //    //console.log("unblinded", unblinded);
+    //}
 
     onSelectDate(date) {
         console.log("onSelectDate()", date);
     }
 
-    //API
-    getTimeSlots() {
-        // [{date: YYYY-MM-DD, times: ["10:00", "14:00"]}
-        return;
-    }
-    getAvailableAppointments() {
-        var today = moment();
-        var firstDayOfMonth = today.startOf("month");
-        var daysInMonth = moment().daysInMonth();
-        var numDates = Math.floor(Math.random() * (daysInMonth - 1)) + 1;
-        var dates = Array(numDates).fill(null).map(function(e, i) {
-                return Math.floor(Math.random() * daysInMonth - 1);
-            })
-        console.log("dates", dates);
-        dates = dates.filter(function(dateNum, pos) {
-                return dates.indexOf(dateNum) == pos;
-            })
-        dates = dates.map(function(dateNum) {
-            return moment().startOf("month").add(dateNum, "days");
-        })
-        console.log("dates", dates);
-        dates = dates.map(function(date) {return date.toDate();})
-        //dates = dates.map(function(date) {return date.toLocaleString();})
-        console.log("dates", dates);
-        return [dates[0], dates[1]];
-    }
+    ////API
+    //getTimeSlots() {
+    //    // [{date: YYYY-MM-DD, times: ["10:00", "14:00"]}
+    //    return;
+    //}
+    //getAvailableAppointments() {
+    //    var today = moment();
+    //    var firstDayOfMonth = today.startOf("month");
+    //    var daysInMonth = moment().daysInMonth();
+    //    var numDates = Math.floor(Math.random() * (daysInMonth - 1)) + 1;
+    //    var dates = Array(numDates).fill(null).map(function(e, i) {
+    //            return Math.floor(Math.random() * daysInMonth - 1);
+    //        })
+    //    console.log("dates", dates);
+    //    dates = dates.filter(function(dateNum, pos) {
+    //            return dates.indexOf(dateNum) == pos;
+    //        })
+    //    dates = dates.map(function(dateNum) {
+    //        return moment().startOf("month").add(dateNum, "days");
+    //    })
+    //    console.log("dates", dates);
+    //    dates = dates.map(function(date) {return date.toDate();})
+    //    //dates = dates.map(function(date) {return date.toLocaleString();})
+    //    console.log("dates", dates);
+    //    return [dates[0], dates[1]];
+    //}
 
-    getTileClassName(date) {
-        
-    }
+    //getTileClassName(date) {
+    //    
+    //}
 
-    scheduleAppointment() {
-        
-    }
+    //scheduleAppointment() {
+    //    
+    //}
 
     getSessionKey() {
     
@@ -94,7 +170,9 @@ class App extends Component {
     pollRegistrar() {
         // receive signed blob from registrar
         // unblind blob from registrar
+        // keep track of key version
     }
+
 
     render() {
 
@@ -104,15 +182,15 @@ class App extends Component {
             <div className="keys">
                 <div className="public-key">
                     <div>Public:</div>
-                    <div className="key">
+                    <textarea className="key">
                         {publicKey}
-                    </div>
+                    </textarea>
                 </div>
                 <div className="private-key">
                     <div>Private:</div>
-                    <div className="key">
+                    <textarea readonly className="key">
                         {privateKey}
-                    </div>
+                    </textarea>
                 </div>
             </div>
         );
@@ -123,14 +201,16 @@ class App extends Component {
                     <h1>Secure Polling System</h1>
                 </header>
                 <div className="content">
-                    <div className="key-generation">
-                        <button onClick={this.generateKeyPair}>Generate Key Pair</button>
+                    <button onClick={this.clearKeys}>Clear Keys</button>
+                    <div className="keys-div">
                         {this.state.keys ? keysDiv : ""}
                     </div>
-
+                    <button onClick={this.blindPublicKey}>Blind Public Key</button>
+                    <div>
+                        {this.state.blindedPublicKey}
+                    </div>
                     <div className="calendar">
                         <Calendar
-                                value={this.getAvailableAppointments()}
                                 minDate={moment().toDate()}
                                 onChange={this.onSelectDate}
                         />
---
+++ src/utils.js
@@ -0,0 +1,175 @@
+import JSEncrypt from "jsencrypt";
+import BlindSignature from "blind-signatures";
+import moment from "moment";
+
+let DEFAULT_KEY_SIZE=2048;
+let LOCAL_STORAGE_KEY="SPSRsaKeys";
+
+export function getOrCreateRSAKeys() {
+
+    var keys;
+    keys = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (keys) {
+        console.log("FOUND KEYS IN LOCAL STORAGE");
+    }
+    if (!keys) {
+        console.log("NO KEYS FOUND; CREATING SOME");
+        var jsencrypt = new JSEncrypt({"default_key_size": DEFAULT_KEY_SIZE})
+        keys = {
+            public: jsencrypt.getPublicKey(),
+            private: jsencrypt.getPrivateKey()
+        };
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(keys));
+    }
+    return keys;
+
+}
+
+export class PublicKeyBlinder {
+
+    PublicKeyBlinder() {
+
+    }
+
+    blind(publicKey) {
+
+        this.key = publicKey;
+        var jsencrypt = new JSEncrypt();
+        jsencrypt.setPublicKey(publicKey);
+        this.jsencrypt = jsencrypt;
+
+        var message = this.key;
+        var N = this.jsencrypt.key.n.toString();
+        var E = this.jsencrypt.key.e.toString();
+
+        var blindData = {
+            message: message,
+            N: N,
+            E: E
+        };
+        console.log("blindData", blindData);
+        const { blinded, r } = BlindSignature.blind(blindData);
+        this.r = r;
+    
+        return blinded;
+
+    }
+
+    unblind(signedKey) {
+
+        var N = this.jsencrypt.key.n.toString();
+        var r = this.r;
+
+        var unblindData = {
+            signed: signedKey,
+            N: N,
+            r: r,
+        };
+
+        const unblinded = BlindSignature.unblind(unblindData);
+
+        this.unblinded = unblinded;
+
+        return unblinded;
+    
+    }
+
+    verify(registrarPublicKey) {
+
+        var unblinded = this.unblinded;
+        var key = registrarPublicKey;
+        var message = this.key;
+
+        var verifyData = {
+            unblinded: unblinded,
+            key: key,
+            message: message,
+        };
+
+        const result2 = BlindSignature.verify2(verifyData);
+
+        return result2;
+
+    }
+}
+
+//export function blindPublicKey(publicKey) {
+//
+//    var message = this.state.keys.public;
+//    var N = this.state.jsencrypt.key.n.toString();
+//    var E = this.state.jsencrypt.key.e.toString();
+//    const { blinded, r } = BlindSignature.blind({
+//        message: message,
+//        N: N,
+//        E: E
+//    });
+//
+//}
+
+function makeid() {
+
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+export function fetchTimeSlots() {
+
+    var today = moment();
+    var firstDayOfMonth = today.startOf("month");
+    var daysInMonth = moment().daysInMonth();
+    var numDates = Math.floor(Math.random() * (daysInMonth - 1)) + 1;
+    var dates = Array(numDates).fill(null).map(function(e, i) {
+            return Math.floor(Math.random() * daysInMonth - 1);
+        })
+    console.log("dates", dates);
+
+    dates = dates.filter(function(dateNum, pos) {
+            return dates.indexOf(dateNum) == pos;
+        })
+
+    dates = dates.map(function(dateNum) {
+        let date = moment().startOf("month").add(dateNum, "days");
+        date.hour(12);
+        return date;
+    })
+    console.log("dates", dates);
+
+    dates = dates.map(function(date) {return date.toDate();})
+    //dates = dates.map(function(date) {return date.toLocaleString();})
+    console.log("dates", dates);
+
+    var timeSlots = dates.map(date => {
+        return {
+            id: makeid(),
+            time: date
+        };
+    })
+
+    return dates.map;
+
+}
+
+
+export function bookAppointment(args) {
+
+    var timeSlotId = args.timeSlotId;
+    var personalId = args.personalId;
+    var blindedKey = args.blindedKey;
+
+    return {
+        id: makeid(),
+        timeSlotId: timeSlotId,
+        personalId: personalId,
+        blindedKey: blindedKey
+    }
+
+}
+
+export function test() {
+
+}
